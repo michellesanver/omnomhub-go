389e28cb566d5b747eeb41ffc3d4b8aadef621a5
Michelle Sanver <michelle.sanver@liip.ch>
2017-07-03 14:27:27 +0200

    Connecting to neo4j

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	bolt "github.com/johnnadratowski/golang-neo4j-bolt-driver"
+)
+
 func main() {
 	// Scrape a recipe from Vegan Rocks
+
+	// Connect to neo4j
+	driver := bolt.NewDriver()
+	// @TODO: Put this in a config
+	conn, err := driver.OpenNeo("bolt://api.omnomhub.dev:7687")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
 }
