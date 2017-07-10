bae99a3c3f091a0d1fa6ef64daf1d683389d4818
Michelle Sanver <michelle.sanver@liip.ch>
2017-07-10 14:35:13 +0200

    Added the possibility to create users via POST.
    This is still a very early prorotype, while learning golang, and obviously a http library would likely be used in the final version, and we would extract to several functions, have config for the DB connection etc. That is all for a later version. For now: YAY! IT WORKS \o/

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # omnomhub-go
-A recipe API written in golang
+A recipe API written in golang. We are using drifter for neo4j. To run it ensure you have initiated your git submodules then to run the API simply do: 
+`go run main.go`
--- main.go
+++ main.go
@@ -2,17 +2,68 @@ package main
 
 import (
 	bolt "github.com/johnnadratowski/golang-neo4j-bolt-driver"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	log "github.com/Sirupsen/logrus"
+	"encoding/json"
 )
 
-func main() {
-	// Scrape a recipe from Vegan Rocks
+type User struct {
+	Name string `json:"name"`
+}
 
-	// Connect to neo4j
+func main() {
+	http.HandleFunc("/user", postUser)
+	log.Fatal(http.ListenAndServe(":8082", nil))
+}
+
+func postUser(rw http.ResponseWriter, req *http.Request) {
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(body))
+
+	var u User
+	err = json.Unmarshal(body, &u)
+
+	if err != nil {
+		panic(err)
+	}
+
+	createUser(u.Name)
+}
+
+func createUser(username string) int64 {
 	driver := bolt.NewDriver()
 	// @TODO: Put this in a config
 	conn, err := driver.OpenNeo("bolt://api.omnomhub.dev:7687")
 	if err != nil {
 		panic(err)
 	}
+
 	defer conn.Close()
+
+	stmt, err := conn.PrepareNeo("CREATE (node:User {name:{name}})")
+	if err != nil {
+		panic(err)
+	}
+
+	result, err := stmt.ExecNeo(map[string]interface{}{"name": username})
+	if err != nil {
+		panic(err)
+	}
+
+	numResult, err := result.RowsAffected()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Created the user: %s\n", username)
+
+	// Closing the statement will also close the rows
+	stmt.Close()
+
+	return numResult
 }
